c938a556bfc67263c30a5aaa1a19fb1c5faf522b
WCWook <wcw703@naver.com>
2022-07-29 18:19:20 +0900

    main 커밋

---
+++ jejuWorkSpace/jejudoPrj/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Main {
+
+	public static int loginNo;
+	public static void main(String[] args) {
+		System.out.println("제주도 좋아~");
+		System.out.println("여기가 메인입니다~");
+	}
+
+}
