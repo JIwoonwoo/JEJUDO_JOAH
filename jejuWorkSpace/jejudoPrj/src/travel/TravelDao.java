ee099f89d9ec975d060f5c95a674737dd68a4845
DESKTOP-2NV54CR\ayj71 <ayj7137@naver.com>
2022-07-29 17:25:37 +0900

    안연주 커밋
    220729

--- jejuWorkSpace/jejudoPrj/src/travel/TravelDao.java
+++ jejuWorkSpace/jejudoPrj/src/travel/TravelDao.java
@@ -4,28 +4,33 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import member.MemberVo;
 import util.JDBCTemplate;
 
 public class TravelDao {
 
 	// 카테고리 1 : 관광지 메소드
-	public TravelVo attraction(int inputnum) throws Exception {
+	public TravelVo attraction(String inputnum) throws Exception {
 		Connection conn = JDBCTemplate.getConnection();
 		
-		String sql = "SELECT P.PURPOSE, C.CATEGORY, TRAVEL_NAME, TRAVEL_ADDRESS FROM TRAVEL T JOIN CATEGORY C ON T.CATEGORY = C.NO JOIN PURPOSE P ON T.THEME = P.NO WHERE T.CATEGORY = 1 ";
+		String sql = "SELECT P.PURPOSE, C.CATEGORY, TRAVEL_NAME, TRAVEL_ADDRESS FROM TRAVEL T JOIN CATEGORY C ON T.CATEGORY = C.NO JOIN PURPOSE P ON T.THEME = P.NO WHERE T.CATEGORY = ? ";
 	
 		PreparedStatement pstmt = conn.prepareStatement(sql);
-//		pstmt.setInt(1, inputnum);
+		pstmt.setString(1, inputnum);
 		
 		ResultSet rs = pstmt.executeQuery();
-		TravelVo vo = null;
+		
+		
 		
 		if(rs.next()) {
-			int catenum = rs.getInt("T.CATEGORY");
+			String purpose = rs.getString("P.PURPOSE");
+			String category = rs.getString("C.CATEGORY");
+			String travelName = rs.getString("TRAVEL_NAME");
+			String travelAddress = rs.getString("TRAVEL_ADDRESS");
+			
+			TravelVo vo = new 
 			
 			
-			vo = new TravelVo();
-			vo.setCategory(catenum);
 		}
 		
 		return vo;
--- jejuWorkSpace/jejudoPrj/src/travel/TravelVo.java
+++ jejuWorkSpace/jejudoPrj/src/travel/TravelVo.java
@@ -4,30 +4,34 @@ import java.sql.Timestamp;
 
 public class TravelVo {
 
-	private int trable_no;
-	private int recommend_type;
-	private int theme;
-	private int category;
+	private String trable_no;
+	private String recommend_type;
+	private String theme;
+	private String category;
 	private String travel_name;
 	private String travel_address;
 	private String activity_yn;
-	private int travel_price;
+	private String travel_price;
 	private String animal_yn;
 	private String travel_info;
 	private String travel_phone;
 	private String open;
 	private String closed;
-	private int like_cnt;
+	private String like_cnt;
 	private String closed_day;
 	private Timestamp delete_date; // 타입 이게 맞나요..?
+	private String purpose;
 	
 	// 기본생성자
 	public void TravelVo() {
 		
 	}
-	
+
 	// 생성자 (파라미터)
-	public TravelVo() {
+	public TravelVo(String trable_no, String recommend_type, String theme, String category, String travel_name,
+			String travel_address, String activity_yn, String travel_price, String animal_yn, String travel_info,
+			String travel_phone, String open, String closed, String like_cnt, String closed_day,
+			Timestamp delete_date, String purpose) {
 		super();
 		this.trable_no = trable_no;
 		this.recommend_type = recommend_type;
@@ -45,170 +49,147 @@ public class TravelVo {
 		this.like_cnt = like_cnt;
 		this.closed_day = closed_day;
 		this.delete_date = delete_date;
+		this.purpose = purpose;
 	}
 
-	// getter setter
-	public int getTrable_no() {
+	public String getTrable_no() {
 		return trable_no;
 	}
 
-
-	public void setTrable_no(int trable_no) {
+	public void setTrable_no(String trable_no) {
 		this.trable_no = trable_no;
 	}
 
-
-	public int getRecommend_type() {
+	public String getRecommend_type() {
 		return recommend_type;
 	}
 
-
-	public void setRecommend_type(int recommend_type) {
+	public void setRecommend_type(String recommend_type) {
 		this.recommend_type = recommend_type;
 	}
 
-
-	public int getTheme() {
+	public String getTheme() {
 		return theme;
 	}
 
-
-	public void setTheme(int theme) {
+	public void setTheme(String theme) {
 		this.theme = theme;
 	}
 
-
-	public int getCategory() {
+	public String getCategory() {
 		return category;
 	}
 
-
-	public void setCategory(int category) {
+	public void setCategory(String category) {
 		this.category = category;
 	}
-
 
 	public String getTravel_name() {
 		return travel_name;
 	}
 
-
 	public void setTravel_name(String travel_name) {
 		this.travel_name = travel_name;
 	}
-
 
 	public String getTravel_address() {
 		return travel_address;
 	}
 
-
 	public void setTravel_address(String travel_address) {
 		this.travel_address = travel_address;
 	}
-
 
 	public String getActivity_yn() {
 		return activity_yn;
 	}
 
-
 	public void setActivity_yn(String activity_yn) {
 		this.activity_yn = activity_yn;
 	}
 
-
-	public int getTravel_price() {
+	public String getTravel_price() {
 		return travel_price;
 	}
 
-
-	public void setTravel_price(int travel_price) {
+	public void setTravel_price(String travel_price) {
 		this.travel_price = travel_price;
 	}
-
 
 	public String getAnimal_yn() {
 		return animal_yn;
 	}
 
-
 	public void setAnimal_yn(String animal_yn) {
 		this.animal_yn = animal_yn;
 	}
-
 
 	public String getTravel_info() {
 		return travel_info;
 	}
 
-
 	public void setTravel_info(String travel_info) {
 		this.travel_info = travel_info;
 	}
-
 
 	public String getTravel_phone() {
 		return travel_phone;
 	}
 
-
 	public void setTravel_phone(String travel_phone) {
 		this.travel_phone = travel_phone;
 	}
-
 
 	public String getOpen() {
 		return open;
 	}
 
-
 	public void setOpen(String open) {
 		this.open = open;
 	}
-
 
 	public String getClosed() {
 		return closed;
 	}
 
-
 	public void setClosed(String closed) {
 		this.closed = closed;
 	}
 
-
-	public int getLike_cnt() {
+	public String getLike_cnt() {
 		return like_cnt;
 	}
 
-
-	public void setLike_cnt(int like_cnt) {
+	public void setLike_cnt(String like_cnt) {
 		this.like_cnt = like_cnt;
 	}
-
 
 	public String getClosed_day() {
 		return closed_day;
 	}
 
-
 	public void setClosed_day(String closed_day) {
 		this.closed_day = closed_day;
 	}
-
 
 	public Timestamp getDelete_date() {
 		return delete_date;
 	}
 
-
 	public void setDelete_date(Timestamp delete_date) {
 		this.delete_date = delete_date;
 	}
+	
+	
 
+	public String getPurpose() {
+		return purpose;
+	}
 
-	// toString
+	public void setPurpose(String purpose) {
+		this.purpose = purpose;
+	}
+
 	@Override
 	public String toString() {
 		return "TravelVo [trable_no=" + trable_no + ", recommend_type=" + recommend_type + ", theme=" + theme
@@ -216,8 +197,13 @@ public class TravelVo {
 				+ ", activity_yn=" + activity_yn + ", travel_price=" + travel_price + ", animal_yn=" + animal_yn
 				+ ", travel_info=" + travel_info + ", travel_phone=" + travel_phone + ", open=" + open + ", closed="
 				+ closed + ", like_cnt=" + like_cnt + ", closed_day=" + closed_day + ", delete_date=" + delete_date
-				+ "]";
+				+ ", purpose=" + purpose + "]";
 	}
+
+	
+	
+
+	
 	
 	
 	
