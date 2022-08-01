cd4a37efe2add3fdb257f62f9c251e01c47975e0
DESKTOP-8SMJJI7\annie <anniemooner@gmail.com>
2022-08-01 13:42:49 +0900

    문동주 0801
    주말 작업 조금..

---
+++ jejuWorkSpace/jejudoPrj/src/flight2/Menu.java
@@ -0,0 +1,17 @@
+package flight2;
+
+import util.InputUtil;
+
+public class Menu {
+
+	public int showMenu() throws Exception {
+		//비행기 예약(reservation), 수정(change), 삭제(delete), 조회(search)
+		System.out.println("1. 비행기 예약 ");
+		System.out.println("2. 비행기 수정 ");
+		System.out.println("3. 비행기 삭제 ");
+		System.out.println("4. 비행기 조회 ");
+
+		return InputUtil.getInt();
+	}
+
+}//class
